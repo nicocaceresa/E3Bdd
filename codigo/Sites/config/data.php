212bd43ea72b6c4c8c242795c3c5d708579af3f8
Daniela Silva Schlack <danielasilvaschlack@MacBook-Pro-de-Daniela.local>
2022-06-19 15:49:54 -0400

    datos bbdd G80 listos

--- codigo/Sites/config/data.php
+++ codigo/Sites/config/data.php
@@ -1,6 +1,11 @@
 <?php
-    // Variables para iniciar sesión
+    // Variables para iniciar sesión G59
     $user = 'grupo59';
     $password = 'basesdegaboynico';
     $db_name = 'grupo59e3';
+
+    // Variables para inicir sesión G80
+    $user2 = 'grupo80';
+    $password2 = 'danisara'
+    $db_name2 = 'grupo80e2';
 ?>
