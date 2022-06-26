2caa923b42a67b8b9577f691e5e73dc61fb140c8
Daniela Silva Schlack <danielasilvaschlack@MacBook-Pro-de-Daniela.local>
2022-06-26 19:07:11 -0400

    plantilla procedimiento almacenado

---
+++ codigo/sql/funciones/hacer_reserva.sql
@@ -0,0 +1,16 @@
+CREATE OR REPLACE FUNCTION 
+
+-- se declara la función y sus argumentos
+hacer_reserva_pasajero(ciudad_origen varchar, ciudad_destino varchar, fecha_salida timestamp, pasaporte_pasajero varchar)
+
+-- se declaea lo que devuelve
+RETURNS void as $$
+
+
+-- se declaran las variables a utilizar en el codigo (de ser necesario)
+
+-- se define la funcion
+BEGIN
+    
+
+END
