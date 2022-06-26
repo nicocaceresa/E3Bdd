fc0d086c146f2229a333ccd38ec9af169dcdf02d
Daniela Silva Schlack <danielasilvaschlack@MacBook-Pro-de-Daniela.local>
2022-06-26 12:50:22 -0400

    modified

--- codigo/Sites/views/vista_reservas.php
+++ codigo/Sites/views/vista_reservas.php
@@ -3,6 +3,17 @@
 $msg = $_GET['msg'];
 session_start();
 
-
-
 ?>
+
+<?php include('../templates/header.html'); ?>
+
+<title> Aquí puedes ver tus Reservas </title>
+
+<body>
+    
+
+
+
+
+<?php include('../templates/footer.html'); ?>
+</body>
