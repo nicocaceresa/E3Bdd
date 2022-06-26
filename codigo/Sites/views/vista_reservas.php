6a7c67220dda6fcbfb66c1c8ee5ef1df48ecaf5b
Daniela Silva Schlack <danielasilvaschlack@MacBook-Pro-de-Daniela.local>
2022-06-26 15:02:38 -0400

    Ver reservas Listo

--- codigo/Sites/views/vista_reservas.php
+++ codigo/Sites/views/vista_reservas.php
@@ -12,9 +12,9 @@ session_start();
     <?php
     $pas = $_SESSION['username'];
     require("../config/conection.php");
-    $query = "SELECT T.pasaporte_pasajero, T.numero_ticket, T.numero_asiento, T.clase, T.comida_y_maleta
-                FROM ticket AS T 
-                WHERE T.pasaporte_pasajero = 'V03976673';";
+    $query = "SELECT T.pasaporte_pasajero, T.reserva_id, T.numero_ticket, T.numero_asiento, T.clase, T.comida_y_maleta, V.vuelo_id, V.estado, V.codigo_compania, V.codigo_aeronave, (A1.nombre, A1.nombre_pais) AS aa1, (A2.nombre, A2.nombre_pais) AS aa2
+                FROM ticket AS T, vuelos AS V, aerodromos AS A1, aerodromos AS A2
+                WHERE T.pasaporte_pasajero = '$pas' AND T.vuelo_id = V.vuelo_id AND V.aerodromo_salida_id = A1.aerodromo_id AND V.aerodromo_llegada_id = A2.aerodromo_id;";
     $result = $db -> prepare($query);
     $result -> execute();
     $data = $result -> fetchAll();
@@ -24,19 +24,17 @@ session_start();
                     <tr>
                         
                         <th> Pasaporte Pasajero </th>
+                        <th> Id Reserva </th>
                         <th> Nº Ticket </th>
                         <th> Asiento </th>
                         <th> Clase </th>
                         <th> Comida/Equipaje </th>
-                        <!--
                         <th> Id Vuelo </th>
                         <th> Estado Vuelo </th>
                         <th> Compañia </th>
                         <th> Aeronave </th>
-                        <th> Aerodromo Origen </th>
-                        <th> País Origen </th>
-                        <th> Aerodromo Destino </th>
-                        <th> País Destino </th> -->
+                        <th> Origen </th>
+                        <th> Destino </th>
 
                     </tr>
 
@@ -48,7 +46,13 @@ session_start();
                                     <td>$d[2]</td>
                                     <td>$d[3]</td>
                                     <td>$d[4]</td>
-
+                                    <td>$d[5]</td>
+                                    <td>$d[6]</td>
+                                    <td>$d[7]</td>
+                                    <td>$d[8]</td>
+                                    <td>$d[9]</td>
+                                    <td>$d[10]</td>
+                                    <td>$d[11]</td>
 
                                 </tr>";
                         }
